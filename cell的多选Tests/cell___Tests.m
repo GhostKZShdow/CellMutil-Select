a960d4b0136067f847c2297705e350905b31776a
我 <KZ@zhangkundemacbook-pro.local>
2015-12-25 13:48:00 +0800

    Initial Commit

---
+++ cell的多选Tests/cell___Tests.m
@@ -0,0 +1,39 @@
+//
+//  cell___Tests.m
+//  cell的多选Tests
+//
+//  Created by 张坤 on 15/12/25.
+//  Copyright © 2015年 KZ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cell___Tests : XCTestCase
+
+@end
+
+@implementation cell___Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
